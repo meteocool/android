031f18f7e600f36508c19ea72ecc430d43d78822
Jeremias Wiedmann <jeremias.wiedmann@mailbox.org>
2020-05-12 23:17:01 +0200

    Added button handler for error

--- app/src/main/java/com/meteocool/ErrorFragment.kt
+++ app/src/main/java/com/meteocool/ErrorFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import org.jetbrains.anko.sdk25.coroutines.onClick
 
 class ErrorFragment : Fragment(){
 
@@ -13,6 +15,13 @@ class ErrorFragment : Fragment(){
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_error, container, false)
+        val view =  inflater.inflate(R.layout.fragment_error, container, false)
+
+        val retryMap = view.findViewById<Button>(R.id.retry_map)
+        retryMap.onClick {
+            requireActivity().supportFragmentManager.popBackStack()
+        }
+
+        return view
     }
 }
