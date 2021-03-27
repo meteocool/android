1d9464bc924efaf62d899f7f489216e0c38a6310
Jeremias Wiedmann <jeremias.wiedmann@mailbox.org>
2021-03-27 11:17:01 +0100

    Solved Privacy policy link #49 (https://github.com/meteocool/android/issues/49) Added privacy policy to intro and settings menu.

--- app/src/main/java/com/meteocool/ui/intro/IntroActivity.kt
+++ app/src/main/java/com/meteocool/ui/intro/IntroActivity.kt
@@ -45,13 +45,8 @@ class IntroActivity : AppIntro2() {
             IntroEnableNotificationsFragment.newInstance()
         )
 
-        addSlide(
-            AppIntroFragment.newInstance(
-                title = getString(R.string.intro_title4),
-                description = getString(R.string.intro_description4),
-                imageDrawable = R.drawable.intro_location,
-            )
-        )
+        /* Custom slide with privacy policy link. Umbrella (Location)  */
+        addSlide(IntroPrivacyPolicyFragment.newInstance())
 
         addSlide(
             AppIntroFragment.newInstance(
---
+++ app/src/main/java/com/meteocool/ui/intro/IntroPrivacyPolicyFragment.kt
@@ -0,0 +1,36 @@
+package com.meteocool.ui.intro
+
+import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.meteocool.R
+import com.meteocool.databinding.IntroPrivacyPolicyBinding
+
+class IntroPrivacyPolicyFragment : Fragment() {
+
+    companion object {
+        fun newInstance(): IntroPrivacyPolicyFragment {
+            return IntroPrivacyPolicyFragment()
+        }
+    }
+
+    private lateinit var viewDataBinding: IntroPrivacyPolicyBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        viewDataBinding =
+            DataBindingUtil.inflate(inflater, R.layout.intro_privacy_policy, container, false)
+        return viewDataBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewDataBinding.description.movementMethod = LinkMovementMethod.getInstance()
+    }
+}
