368dcbf3372d6025ed21ceeb4ddfdd930b098741
Jochen Sprickerhof <git@jochen.sprickerhof.de>
2021-08-17 22:50:27 +0200

    MyFirebaseMessagingService must be a Service

--- app/src/fdroid/java/com/meteocool/firebase/MyFirebaseMessagingService.kt
+++ app/src/fdroid/java/com/meteocool/firebase/MyFirebaseMessagingService.kt
@@ -1,12 +1,29 @@
 package com.meteocool.firebase
 
+import android.app.Service
 import android.content.Context
+import android.content.Intent
+import android.os.IBinder
 
 
-class MyFirebaseMessagingService {
+class MyFirebaseMessagingService : Service() {
 
     companion object {
-        fun cancelNotification(context: Context, from : String) {
+        fun cancelNotification(context: Context, from: String) {
         }
+    }
+
+    override fun onCreate() {
+    }
+
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        return START_NOT_STICKY;
+    }
+
+    override fun onBind(intent: Intent): IBinder? {
+        return null
+    }
+
+    override fun onDestroy() {
     }
 }
