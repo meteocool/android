ac11e5c21d65012378a406b49d94e2ea3f170898
Jeremias Wiedmann <jeremias.wiedmann@mailbox.org>
2022-01-07 00:21:18 +0100

    Also add LocationListener to gms build for compatibility. Tidying up required

---
+++ app/src/gms/java/com/meteocool/location/LocationListener.kt
@@ -0,0 +1,59 @@
+package com.meteocool.location
+
+import android.content.Context
+import android.location.Location
+import android.os.Bundle
+import androidx.work.WorkManager
+import com.meteocool.location.storage.LocationPersistenceWorker
+import com.yayandroid.locationmanager.listener.LocationListener
+import timber.log.Timber
+
+class LocationListener(val context: Context) : LocationListener {
+    override fun onProcessTypeChanged(processType: Int) {
+        return
+    }
+
+    override fun onLocationChanged(location: Location?) {
+        Timber.d("Location: $location")
+        if (location !== null) {
+            handleLocation(location)
+        }
+    }
+
+    override fun onLocationFailed(type: Int) {
+        return
+    }
+
+    override fun onPermissionGranted(alreadyHadPermission: Boolean) {
+        return
+    }
+
+    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+        return
+    }
+
+    override fun onProviderEnabled(provider: String?) {
+        return
+    }
+
+    override fun onProviderDisabled(provider: String?) {
+        return
+    }
+
+    /**
+     * Persist location to database and upload it to the server.
+     * @param location to persist and upload
+     */
+    private fun handleLocation(
+        location: Location,
+    ) {
+        WorkManager.getInstance(context)
+            .enqueue(
+                listOf(
+                    LocationPersistenceWorker.createRequest(
+                        LocationPersistenceWorker.createMeteocooLocationData(location)
+                    ),
+                )
+            )
+    }
+}
